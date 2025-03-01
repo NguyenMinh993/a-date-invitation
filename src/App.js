11405f0b6036ec3dc79645953274726704881e59
NguyenMinh993 <nguyenminh090903@gmail.com>
2025-03-02 03:34:20 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import InvitationPage from './InvitationPage'; // Trang thư mời hiện tại
-import HomePage from './HomePage'; // Trang khởi đầu mới
+import InvitationPage from './InvitationPage';
+import HomePage from './HomePage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/a-date-invitation">
       <div className="App">
         <Routes>
-          <Route path="/a-date-invitation" element={<HomePage />} />
-          <Route path="/" element={<InvitationPage />} />
+          <Route path="/" element={<HomePage />} /> {/* Default page at /a-date-invitation/ */}
+          <Route path="/invitation" element={<InvitationPage />} /> {/* Page at /a-date-invitation/invitation */}
         </Routes>
       </div>
     </Router>
