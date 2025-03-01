03817a7a0655affbb33e32c640395d860bf2c90b
NguyenMinh993 <nguyenminh090903@gmail.com>
2025-03-02 00:48:15 +0700

    okk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import './App.css';
-
+import Pong from './Pong.jpg';
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <header className="App-header">
         <h1>A Special Date Invitation</h1>
         <div className="image-container">
-        <img src="/Pong.jpg" alt="Couple" className="date-image" />
+          <img src={Pong} alt="Couple" className="date-image" />
         </div>
-        <p className="greeting">Hey [Tên bạn gái],</p>
+        <p className="greeting">Hey Võ Ngọc Hoa,</p>
         <p className="invitation">
-          Would you like to go on a with me this weekend? Maybe dinner and a movie? Let me know! 💕
+          Would you like to go on a date with me this weekend? Maybe dinner and a movie? Let me know! 💕
         </p>
-        <div className="image-container">
-          <img src="/clam.png" alt="Couple" className="date-image" />
-          <a href="https://maps.app.goo.gl/zsXp8JrivXPUaPx96">Địa chỉ</a>
-        </div>
         <p className="signature">Love, [Tên bạn]</p>
-      </div>
+      </header>
     </div>
   );
 }
